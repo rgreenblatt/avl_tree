607511d2101942417844c30e18244a5e1a7c3f8a
rgreenblatt <greenblattryan@gmail.com>
2023-09-27 16:18:48 -0700

    avl tree

---
+++ src/lib.rs
@@ -0,0 +1,53 @@
+use ordered_float::OrderedFloat;
+use pyo3::prelude::*;
+
+mod avl;
+
+#[pymodule]
+fn avl_tree(_py: Python, m: &PyModule) -> PyResult<()> {
+  m.add_class::<PyAVLTree>()?;
+  Ok(())
+}
+
+#[pyclass]
+pub struct PyAVLTree {
+  tree: avl::AVLTree<OrderedFloat<f64>>,
+}
+
+#[pymethods]
+impl PyAVLTree {
+  #[new]
+  fn new() -> Self {
+    PyAVLTree {
+      tree: avl::AVLTree::new(),
+    }
+  }
+
+  fn insert(&mut self, value: f64) -> bool {
+    self.tree.insert(value.into())
+  }
+
+  fn remove(&mut self, value: f64) -> bool {
+    self.tree.remove(&value.into())
+  }
+
+  fn contains(&self, value: f64) -> bool {
+    self.tree.contains(&value.into())
+  }
+
+  fn len(&self) -> usize {
+    self.tree.len()
+  }
+
+  fn is_empty(&self) -> bool {
+    self.tree.is_empty()
+  }
+
+  fn rank(&self, value: f64) -> usize {
+    self.tree.rank(&value.into())
+  }
+
+  fn select(&self, rank: usize) -> Option<f64> {
+    self.tree.select(rank).cloned().map(|x| x.into_inner())
+  }
+}
